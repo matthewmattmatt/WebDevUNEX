170c7a83fcd7f2ba2f4bbc40ff60b3b0d7b7b79e
Matt Loehrer <matt.matt.matt.matt@gmail.com>
2017-12-02 12:01:06 -0800

    new assignment

---
+++ Class6/cartOrder.js
@@ -0,0 +1,16 @@
+
+<script>
+'use strict'
+function customer(name, address) {
+	this._name = name;
+	this._address = address;
+
+}
+
+var myCustomer = new customer ("Fred", "123 Fake Street");
+
+</script>
+
+
+
+
